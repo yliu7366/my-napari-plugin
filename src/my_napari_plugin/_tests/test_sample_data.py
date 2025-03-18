0cacccb402bb21094a1b44b06ab26b3744b3af12
Liu <liuy5@l02334410.niaid.nih.gov>
2025-03-18 15:43:32 -0400

    initial commit

---
+++ src/my_napari_plugin/_tests/test_sample_data.py
@@ -0,0 +1,7 @@
+# from my_napari_plugin import make_sample_data
+
+# add your tests here...
+
+
+def test_something():
+    pass
---
+++ src/my_napari_plugin/_tests/test_writer.py
@@ -0,0 +1,7 @@
+# from my_napari_plugin import write_single_image, write_multiple
+
+# add your tests here...
+
+
+def test_something():
+    pass
